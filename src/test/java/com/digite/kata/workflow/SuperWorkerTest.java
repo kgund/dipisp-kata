86e71bcae7af8817f1fe91c52cc318c86e690948
kgund <kgund@digite.com>
2021-02-02 00:48:27 +0530

    Tests for SuperWorker

---
+++ src/test/java/com/digite/kata/workflow/SuperWorkerTest.java
@@ -0,0 +1,26 @@
+package com.digite.kata.workflow;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SuperWorkerTest {
+    SuperWorker worker;
+
+    @BeforeEach
+    public void setup() {
+        worker = new SuperWorker();
+    }
+
+    @Test
+    public void testSuperWorkerEats() {
+        String eats = worker.eat();
+        Assertions.assertEquals("SuperWorker Eating", eats);
+    }
+
+    @Test
+    public void testSuperWorkerWorks() {
+        String works = worker.work();
+        Assertions.assertEquals("SuperWorker Working",works);
+    }
+}
