2bd2f0e39507bc6c1fd1d77bd372f3c9721b5586
kgund <kgund@digite.com>
2021-02-02 08:50:38 +0530

    Refactoring WeatherTracker

--- src/main/java/com/digite/kata/tracker/NotificationGenerator.java
+++ src/main/java/com/digite/kata/tracker/NotificationGenerator.java
@@ -1,4 +1,6 @@
 package com.digite.kata.tracker;
 
 public interface NotificationGenerator {
+    public String generateWeatherAlert(String weatherConditions);
 }
+
--- src/main/java/com/digite/kata/tracker/WeatherTracker.java
+++ src/main/java/com/digite/kata/tracker/WeatherTracker.java
@@ -1,25 +1,8 @@
 package com.digite.kata.tracker;
 
 public class WeatherTracker {
-    String currentConditions;
-    Phone phone;
-    Emailer emailer;
-
-    public WeatherTracker() {
-        phone = new Phone();
-        emailer = new Emailer();
-    }
-
+    
     public String setCurrentConditions(NotificationGenerator generator, String weatherDescription) {
-        this.currentConditions = weatherDescription;
-        if (weatherDescription == "rainy") {
-            String alert = phone.generateWeatherAlert(weatherDescription);
-            System.out.print(alert);
-        }
-        if (weatherDescription == "sunny") {
-            String alert = emailer.generateWeatherAlert(weatherDescription);
-            System.out.print(alert);
-        }
-        return null;
+        return generator.generateWeatherAlert(weatherDescription);
     }
 }
